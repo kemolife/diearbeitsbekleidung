1b25981068a8155ca27f4f57c3dbb4895626509a
telesyk <telesyk89@yahoo.com>
2018-04-10 17:31:49 +0300

    add beruf section | upadte print section

--- themes/ownertheme/src/js/app/app.js
+++ themes/ownertheme/src/js/app/app.js
@@ -1,4 +1,6 @@
 /* Main JS */
+// chenge default buttons in slick.js
+const slick = require('slick-carousel-browserify');
 
 (function() {
     
@@ -8,7 +10,9 @@
 		navClass:		'.navbar',
 		navExtended:	'navbar__extended',
 		navToggler:		'.navbar-toggler',
-		navClose:		'navbar-close'
+		navClose:		'navbar-close',
+		// home beruf
+		berufList:		'.section__beruf--list'
 	};
 	
 	/*$(app.navToggler).toggle(function() {
@@ -30,5 +34,18 @@
 	/*$('.'+app.navClose).on('click', function() {
 		$(this).removeClass(app.navClose).closest('.col-4').siblings('.col-4').fadeIn();
 	});*/
+	
+	
+	if(window.innerWidth > 1199) {
+		if($(app.berufList.length)) {
+			slick($(app.berufList), {
+				variableWidth: true,
+				slidesToShow: 3,
+				speed: 500
+			});
+		}
+	} else {
+			console.log('window not 1200px');
+		}
 	
 })(console.log('app.js loaded'));
