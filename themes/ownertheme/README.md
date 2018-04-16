89cf64c2146ddad3066982dad54842e707e74198
telesyk <telesyk89@yahoo.com>
2018-04-16 18:07:42 +0300

    add intro slider

--- themes/ownertheme/README.md
+++ themes/ownertheme/README.md
@@ -4,7 +4,7 @@ used plugins:
 * jquery 3
 * bootstrap 4
 * material design icons
----
 * slick carousel
+---
 * animate.css
 * wow.js
--- themes/ownertheme/src/js/app/app.js
+++ themes/ownertheme/src/js/app/app.js
@@ -13,7 +13,9 @@ const slick = require('slick-carousel-browserify');
 		navClose:		'navbar-close',
 		// home beruf
 		berufList:		'.section__beruf--list',
-		homeCategories:	'.section__top-categories--list'
+		homeCategories:	'.section__top-categories--list',
+		//
+		introSlider:	'.intro__slider'
 	};
 	
 	/*$(app.navToggler).toggle(function() {
@@ -35,6 +37,23 @@ const slick = require('slick-carousel-browserify');
 	/*$('.'+app.navClose).on('click', function() {
 		$(this).removeClass(app.navClose).closest('.col-4').siblings('.col-4').fadeIn();
 	});*/
+	
+	
+	if($(app.introSlider.length)) {
+		slick($(app.introSlider), {
+			slidesToShow: 1,
+			dots: true,
+			arrows: false,
+			mobileFirst: true,
+			responsive: [{
+				breakpoint: 1199,
+				settings: {
+					arrows: true,
+					dots: false
+				}
+			}]
+		});
+	}
 	
 	
 	if(window.innerWidth > 1199) {
